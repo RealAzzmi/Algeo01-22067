614cf18599014ea0c98f60977b69057e09f6e28c
Azmi <azmibazeid@gmail.com>
2023-09-19 23:03:52 +0700

    Create Application.java

---
+++ Application.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+import linearalgebra.LinearSystem;
+import linearalgebra.Matrix;
+
+public class Application {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int row_count, col_count;
+        row_count = input.nextInt();
+        col_count = input.nextInt();
+
+        Matrix augMatrix = new Matrix(row_count, col_count);
+        for (int i = 0; i < row_count; ++i) {
+            for (int j = 0; j < col_count; ++j) {
+                augMatrix.matrix[i][j] = input.nextDouble();
+            }
+        }
+
+        LinearSystem ls = new LinearSystem(augMatrix);
+        // ls.augmentedMatrix.print();
+        // System.out.println();
+        // System.out.println();
+
+        ls.gaussJordan();
+        Matrix solution = ls.gaussJordan();
+        solution.print();
+
+        input.close();
+    }
+}
