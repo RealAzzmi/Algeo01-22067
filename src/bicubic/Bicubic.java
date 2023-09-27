45a0d28dcb738d2cd9600212ab4954e237f860ec
Azmi <azmibazeid@gmail.com>
2023-09-27 15:39:32 +0700

    Fixed Bicubic Interpolation

--- src/bicubic/Bicubic.java
+++ src/bicubic/Bicubic.java
@@ -1,39 +1,34 @@
 package bicubic;
 import linearalgebra.Matrix;
 public class Bicubic{
-    public static double solveBicubic(Matrix inputMatrix,double a, double b){
-        double[][] bicubicValues = {
-        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-        {1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0},
-        {1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0},
-        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-        {0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-        {0,1,2,3,0,0,0,0,0,0,0,0,0,0,0,0},
-        {0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-        {0,1,2,3,0,1,2,3,0,1,2,3,0,1,2,3},
-        {0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0},
-        {0,0,0,0,1,0,0,0,2,0,0,0,3,0,0,0},
-        {0,0,0,0,1,1,1,1,2,2,2,2,3,3,3,3},
-        {0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,1,2,3,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,1,0,0,0,2,0,0,0,3,0,0},
-        {0,0,0,0,0,1,2,3,0,2,4,6,0,3,6,9}         
-    };Matrix bicubicMatrix = new Matrix(bicubicValues);
-    Matrix mInverse= Matrix.inverse(bicubicMatrix);
-    Matrix solution = Matrix.multiplyMatrix(mInverse,inputMatrix);
-    double sol = 0;
-    for(int i=0;i<16;i++){
-            if(i<4){
-            sol+= solution.matrix[i][0]*Math.pow(a,i);
-            }else if(i<8){
-                sol+=solution.matrix[i-4][1]*Math.pow(a,i-4)*b;
-            }else if(i<12){
-                sol+= solution.matrix[i-8][2]*Math.pow(a,i-8)*b*b;
-            }else{
-                sol+= solution.matrix[i-12][3]*Math.pow(a,i-12)*b*b*b;
+    public static Matrix xMatrix = new Matrix(new double[][] {
+            {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0},
+            {1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0},
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,1,2,3,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0},
+            {0,1,2,3,0,1,2,3,0,1,2,3,0,1,2,3},
+            {0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0},
+            {0,0,0,0,1,0,0,0,2,0,0,0,3,0,0,0},
+            {0,0,0,0,1,1,1,1,2,2,2,2,3,3,3,3},
+            {0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,1,2,3,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,1,0,0,0,2,0, 0,0,3,0,0},
+            {0,0,0,0,0,1,2,3,0,2,4,6,0,3,6,9},     
+    });
+    public static Matrix dMatrix = xMatrix.inverse();
+    
+    public static double approximate(Matrix inputMatrix, double x, double y) {
+        Matrix coefficients = Matrix.multiplyMatrix(dMatrix, inputMatrix);
+        double approximation = 0;
+        for (int yExp = 0; yExp < 4; ++yExp) {
+            for (int xExp = 0; xExp < 4; ++xExp) {
+                approximation += coefficients.matrix[4 * yExp + xExp][0] * Math.pow(x, xExp) * Math.pow(y, yExp);
             }
         }
-    return sol;
-}
+        return approximation;
+    }
 }
